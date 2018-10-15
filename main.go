9515ffb972b6acf1f7d83b429fd571908a735f0d
jiezhu <jiezhu@tencent.com>
2018-10-15 20:05:59 +0800

    parameters in path

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+
+	r.GET("/usr/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello, %s", name)
 	})
+
+	r.GET("/usr/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		msg := name + " is " + action
+		c.String(http.StatusOK, msg)
+	})
+
 	r.Run()
 }
