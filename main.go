3fed57af2ecd14b5aef51d7b41196c679ddfe5b9
jiezhu <jiezhu@tencent.com>
2018-10-16 11:04:37 +0800

    upload files

--- main.go
+++ main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
 func main() {
 	r := gin.Default()
 
-	r.POST("/post", func(c *gin.Context) {
-		id := c.Query("id")
-		page := c.DefaultQuery("page", "0")
-		name := c.PostForm("name")
-		msg := c.PostForm("msg")
+	r.POST("/upload", func(c *gin.Context) {
+		form, _ := c.MultipartForm()
+		files := form.File["upload[]"]
 
-		fmt.Printf("id: %s; page: %s; name: %s; message: %s\n", id, page, name, msg)
+		for _, file := range files {
+			log.Println(file.Filename)
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
 	})
 
-	r.Run(":8081")
+	r.Run(":8080")
 }
